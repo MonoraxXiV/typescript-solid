d854036bf8f8f3b08b2af6d6821295d39c33b6d8
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-12-01 10:11:21 +0100

    finished 1.O

---
+++ SOLID/1.O/old.ts
@@ -0,0 +1,117 @@
+class Dog {
+    private _name;
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get type() {
+        return 'dog';
+    }
+
+    get sound(){
+        return 'Woef'
+    }
+}
+
+class Cat {
+    private _name;
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get type() {
+        return 'cat';
+    }
+
+    get makeSound(){
+        return 'Meow'
+    }
+}
+
+class Parrot {
+    private _name;
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get type() {
+        return 'parrot';
+    }
+    get makeSound(){
+        return 'I am a pirate'
+    }
+}
+
+class Bat{
+    private _name;
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get type() {
+        return 'bat';
+    }
+
+    get makeSound(){
+        return 'screeeech'
+    }
+}
+
+class Zoo {
+    private _animals : Array<Object> = new Array<Object>();
+
+    public addAnimal(animal: object) {
+        this._animals.push(animal);
+    }
+
+    get animals(): Array<Object> {
+        return this._animals;
+    }
+/*
+    public makeSound(animal: object) : string {
+        switch(animal.type) {
+            case 'cat':
+                return 'Miauw';
+            case 'dog':
+                return 'Woef';
+            case 'parrot':
+                return 'I am a pirate';
+            case 'bat':
+                return 'screeeech';
+
+            default:
+                throw new Error('Unknown type: '+ animal.type);
+        }
+    }
+
+ */
+}
+let zoo = new Zoo;
+zoo.addAnimal(new Cat);
+zoo.addAnimal(new Dog);
+zoo.addAnimal(new Parrot);
+zoo.addAnimal(new Bat);
+
+zoo.animals.forEach((animal) => {
+    document.querySelector('#target').innerHTML += (animal.type + ": " + animal.makeSound + "<br>");
+});
